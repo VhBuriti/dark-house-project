e726a3ab9baa1d3f3c8ad4d35cf3d42f816ceb6b
VhBuriti <victorhugoburit@hotmail.com>
2024-04-22 15:02:57 -0300

    chore: adding and fixing styles

--- client/src/components/sections/PostCard/PostCard.tsx
+++ client/src/components/sections/PostCard/PostCard.tsx
@@ -6,14 +6,19 @@ export default function PostCard() {
     return(
         <div className="post-card__container">
             <div className="post-card__author-details">
-                <img src="" alt="" />
-                <p>Author Place Holder</p>
+                <img src={require("../../../assets/placeholder.webp")} alt="" className="post-card__avatar"/>
+                <p className="post-card__author">Giovanni Colantonio</p>
             </div>
-            <div className="post-card__title">
-                <p>Everything I Know About Creating Buzz, I Learned From Taylor Swift</p>
+            <div>
+                <p className="post-card__title">Everything I Know About Creating Buzz, I Learned From Taylor Swift</p>
             </div>
-            <div className="post-card__description">
-                <p>Scarcity is the one thing you can never have enough of.</p>
+            <div>
+                <p className="post-card__description">Scarcity is the one thing you can never have enough of.</p>
+            </div>
+            <div className="post-card__additional-properties">
+                <p className="post-card__read-time">5 min read</p><span>.</span>
+                <p className="post-card__publish-date">Feb 21, 2017</p><span>.</span>
+                <p className="post-card__article-type">Economics</p>
             </div>
         </div>
     )
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/global.scss'
 import NavBar from './components/common/NavBar';
 import TopBar from './components/common/TopBar';
+import PostCard from './components/sections/PostCard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,5 +12,6 @@ root.render(
   <React.StrictMode>
     <TopBar />
     <NavBar />
+    <PostCard />
   </React.StrictMode>
 );
