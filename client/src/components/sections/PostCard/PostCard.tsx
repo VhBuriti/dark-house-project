f0b912785ca246e1d7dd0e5ac370d4cae6bfd0b0
VhBuriti <victorhugoburit@hotmail.com>
2024-05-28 16:56:56 -0300

    feature: adding post image to cards

--- client/src/components/sections/PostCard/PostCard.tsx
+++ client/src/components/sections/PostCard/PostCard.tsx
@@ -1,32 +1,43 @@
 import React from "react";
-import '../../../styles/postCard.scss'
+import "../../../styles/postCard.scss";
 
-export default function PostCard({postContent}: any) {
+export default function PostCard({ postContent }: any) {
+  const { author, title, content, postDate, category, _id, postThumb } =
+    postContent;
+  const postUrl = title
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]/g, "");
 
-    const { author, title, content, postDate, category, _id } = postContent
-    const postUrl = title.toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]/g, '');
-
-    return(
-        <div className="post-card__container">
-            <a href={`/story/${postUrl}-${_id}`}>
-            <div className="post-card__author-details">
-                <img src={require("../../../assets/placeholder.webp")} alt="" className="post-card__avatar"/>
-                <p className="post-card__author">{author}</p>
-            </div>
-            <div>
-                <p className="post-card__title">{title}</p>
-            </div>
-            <div>
-                <p className="post-card__description">{content}</p>
-            </div>
-            <div className="post-card__additional-properties">
-                <p className="post-card__read-time">5 min read</p><span>.</span>
-                <p className="post-card__publish-date">{postDate}</p><span>.</span>
-                {category && (
-                    <p className="post-card__article-type">{category}</p>
-                )}
-            </div>
-            </a>
+  return (
+    <div className="post-card__container">
+      <a href={`/story/${postUrl}-${_id}`}>
+        <div className="post-card__author-details">
+          <img
+            src={require("../../../assets/placeholder.webp")}
+            alt=""
+            className="post-card__avatar"
+          />
+          <p className="post-card__author">{author}</p>
         </div>
-    )
+        <div>
+          <p className="post-card__title">{title}</p>
+        </div>
+        <div>
+          <p className="post-card__description">{content}</p>
+        </div>
+        <div className="post-card__additional-properties">
+          <p className="post-card__read-time">5 min read</p>
+          <span>.</span>
+          <p className="post-card__publish-date">{postDate}</p>
+          <span>.</span>
+          {category && <p className="post-card__article-type">{category}</p>}
+        </div>
+      </a>
+
+      <div>
+        <img src={postThumb} />
+      </div>
+    </div>
+  );
 }
