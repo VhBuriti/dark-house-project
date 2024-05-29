fe9e5a289477d5ce81fb6d732110919f95f67d3c
VhBuriti <victorhugoburit@hotmail.com>
2024-05-29 12:15:25 -0300

    feature: styling pages and cards

--- client/src/components/sections/PostCard/PostCard.tsx
+++ client/src/components/sections/PostCard/PostCard.tsx
@@ -11,32 +11,34 @@ export default function PostCard({ postContent }: any) {
 
   return (
     <div className="post-card__container">
-      <a href={`/story/${postUrl}-${_id}`}>
-        <div className="post-card__author-details">
-          <img
-            src={require("../../../assets/placeholder.webp")}
-            alt=""
-            className="post-card__avatar"
-          />
-          <p className="post-card__author">{author}</p>
-        </div>
-        <div>
-          <p className="post-card__title">{title}</p>
-        </div>
-        <div>
-          <p className="post-card__description">{content}</p>
-        </div>
-        <div className="post-card__additional-properties">
-          <p className="post-card__read-time">5 min read</p>
-          <span>.</span>
-          <p className="post-card__publish-date">{postDate}</p>
-          <span>.</span>
-          {category && <p className="post-card__article-type">{category}</p>}
-        </div>
-      </a>
+      <div className="post-card__text-container">
+        <a href={`/story/${postUrl}-${_id}`}>
+          <div className="post-card__author-details">
+            <img
+              src={require("../../../assets/placeholder.webp")}
+              alt=""
+              className="post-card__avatar"
+            />
+            <p className="post-card__author">{author}</p>
+          </div>
+          <div>
+            <p className="post-card__title">{title}</p>
+          </div>
+          <div>
+            <p className="post-card__description">{content}</p>
+          </div>
+          <div className="post-card__additional-properties">
+            <p className="post-card__read-time">5 min read</p>
+            <span>.</span>
+            <p className="post-card__publish-date">{postDate}</p>
+            <span>.</span>
+            {category && <p className="post-card__article-type">{category}</p>}
+          </div>
+        </a>
+      </div>
 
       <div>
-        <img src={postThumb} />
+        <img src={postThumb} className="post-card__thumb" />
       </div>
     </div>
   );
--- client/src/pages/PostPage/index.tsx
+++ client/src/pages/PostPage/index.tsx
@@ -1,6 +1,8 @@
 import { GetPost } from "../../utils/getPost";
 import { useEffect, useState } from "react";
 
+import "../../styles/postPage.scss";
+
 export default function PostPage() {
   const [post, setPost]: any = useState({});
 
@@ -22,18 +24,27 @@ export default function PostPage() {
   console.log("content", paragraphs);
 
   return (
-    <>
-      <h2>{title}</h2>
-      <img src={postThumb} />
-      <div>
-        <span>{author}·</span>
-        <span>{postDate}·</span>
-        <span>{category}</span>
-      </div>
+    <div className="post-page__container">
+      <div className="post-page__inner-container">
+        <div>
+          <div className="post-page__title-container">
+            <h2>{title}</h2>
+          </div>
+          <div>
+            <span>Written by: {author}</span>
+          </div>
+          <div>
+          <span>{postDate}·</span>
+          <span>{category}</span>
+        </div>
+        </div>
+        <img src={postThumb} />
 
-      {paragraphs?.map((paragraph: string) => (
-        <p>{paragraph}</p>
-      ))}
-    </>
+
+        {paragraphs?.map((paragraph: string) => (
+          <p>{paragraph}</p>
+        ))}
+      </div>
+    </div>
   );
 }
